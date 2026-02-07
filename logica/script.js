1d841a8d5987eb050bde242e3d6b585f11004b8b
ToniLisboa <tonilisboa.souza@gmail.com>
2026-02-07 20:28:58 -0300

    Iniciando script.js
    Banco de Materiais
    Tamanhos Padrão

---
+++ logica/script.js
@@ -0,0 +1,20 @@
+// Banco de Materiais
+const MATERIAIS = {
+    "Acrilico": 1.19,
+    "Petg Cristal": 1.27,
+    "PS": 1.07,
+    "PP": 0.95,
+    "PVC": 1.33,
+    "MDF (Aprox)": 0.75,
+    "ACM (Aprox)": 1.35,
+    "Outro": 1.00
+};
+
+// Tamanhos Padrão
+const TAMANHOS_PADRAO = {
+    "personalizado": [0, 0],
+    "200x100": [200, 100],
+    "300x200": [300, 200],
+    "122x244": [122, 244],
+    "100x100": [100, 100]
+};
